a0976e5fd0862cb1fe648a3d47b556e1214f13e0
Kadir <kadirincedev@gmail.com>
2020-07-25 20:01:12 +0300

    Completed App

--- Weight on Planets/ContentView.swift
+++ Weight on Planets/ContentView.swift
@@ -17,7 +17,6 @@
  Saturn: 1.06
  Uranus: 0.92
  Neptune: 1.19
- Pluto: 0.06
 
  weight = mass x surface gravity,
  */
@@ -28,16 +27,32 @@ struct ContentView: View {
     var planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
     @State private var selectedPlanet = 2
     @State private var weight = 70
-    
+
+    func calculateWeight() -> Double {
+        switch selectedPlanet {
+        case 0:
+            return (Double(weight) * 0.38)
+        case 1:
+            return (Double(weight) * 0.91)
+        case 2:
+            return (Double(weight) * 1.00)
+        case 3:
+            return (Double(weight) * 0.38)
+        case 4:
+            return (Double(weight) * 2.34)
+        case 5:
+            return (Double(weight) * 1.06)
+        case 6:
+            return (Double(weight) * 0.92)
+        case 7:
+            return (Double(weight) * 1.19)
+        default:
+            print("")
+        }
+        return 0
+    }
 
     var body: some View {
-        /*
-         imageView --> changing with picker (image of planet)
-         Picker --> planets' name
-         Text --> Result
-         Textfield
-         Calculate Button
-         */
 
         ZStack {
             Color.black
@@ -61,7 +76,7 @@ struct ContentView: View {
                     .frame(width: UIScreen.main.bounds.width / 3, height: 100, alignment: .center)
                     Spacer()
                     Picker(selection: $weight, label: Text("")) {
-                        ForEach(0 ..< 200) { i in
+                        ForEach(0 ..< 500) { i in
                             Text("\(i)").foregroundColor(.white)
                         }
                     }
@@ -71,7 +86,7 @@ struct ContentView: View {
                 }
                 .padding(.top, 60)
                 Spacer()
-                Text("Weight is 70 kg on \(planets[self.selectedPlanet])")
+                Text("Weight is \(calculateWeight(), specifier: "%.1f") kg on \(planets[self.selectedPlanet])")
                     .foregroundColor(.white)
                     .font(Font.system(size: 24))
                 Spacer()
