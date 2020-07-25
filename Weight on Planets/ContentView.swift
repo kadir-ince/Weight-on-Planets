8f408209007b3c535e1237c76795a2e2a3aaf7d3
Kadir <kadirincedev@gmail.com>
2020-07-25 18:17:55 +0300

    Initial Commit

---
+++ Weight on Planets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weight on Planets
+//
+//  Created by Kadir on 25.07.2020.
+//  Copyright © 2020 Kadir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
